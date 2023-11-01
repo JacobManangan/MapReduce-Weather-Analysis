f697d23a6efff33326087b6654bd0f0d7d502ad7
JacobManangan <64616113+JacobManangan@users.noreply.github.com>
2023-11-01 18:10:49 -0400

    Update AvgTemperatureMapper.java

--- AvgTemperatureMapper.java
+++ AvgTemperatureMapper.java
@@ -13,21 +13,6 @@ extends Mapper<LongWritable, Text, Text, IntWritable> {
 
 private static final int MISSING = 9999;
 
-/*
- * 
- * 
-Now we have the the weather data set (same format as Lecture 7 page 3).
-What are the average air temperatures at 11pm each month? Please disregard the lines with missing temperature values and invalid quality codes. In the output, each line should be the year and month (e.g. 195001), followed by the average. 
-Use the given code to implement your map and reduce functions. 
-
-In A1, the provided data sets do not have temperature recorded for 11pm. There are two options here:
-
-1. If you have already completed Assignment 1, and it currently uses 11pm for temperature data, you can keep it as it is.
-
-2. If you are still working on Assignment 1, you have the option to generate the average temperature for 1pm instead of using 11pm.
- */
-
-
 
 @Override
 public void map(LongWritable key, Text value, Context context)
